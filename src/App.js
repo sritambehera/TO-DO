d03b0f41472728232bcbc559d24dbe6a742a9576
Sritam Behera <sritambehera110@gmail.com>
2019-11-25 19:48:04 +0530

    simple todo app using reactjs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import todoData from './todoData';
+import Todoitem from './Todoitem';
 
-function App() {
-  return (
+class App extends React.Component {
+	constructor(){
+		super()
+		this.state = {
+			todos: todoData
+		}
+		this.handleChange = this.handleChange.bind(this)
+	}
+
+	handleChange(id) {
+		this.setState(prevState => {
+			const updated = prevState.todos.map(todo => {
+                if(todo.id === id){
+                	todo.completed = !todo.completed
+                }
+                return todo;
+            }
+				)
+			return {
+				todos : updated
+			}
+		}
+		
+		)
+		
+	}
+	render() {
+    const todoItems = this.state.todos.map(items => <Todoitem 
+    	key={items.id} 
+    	item= {items}  
+    	handleChange={this.handleChange}
+    	/>)
+    return (
     <div className="App">
-      <header className="App-header">
+        {todoItems}
+
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
     </div>
-  );
+  )
+
+	}
 }
+
+
 
 export default App;
---
+++ src/todoData.js
@@ -0,0 +1,18 @@
+const todoData = [
+{
+	id : 1,
+	text : "learned reactjs",
+	completed : true
+},
+{
+	id : 2,
+	text :"one issue resolved" ,
+	completed : false
+},
+{
+	id : 3,
+	text : "idea for WOC",
+	completed : false,
+}
+]
+export default todoData
